3e4f5bf9d00d0b3b9e3f9b4280ba91d0084c32f6
zubby2528-hue <zubby2528@gmail.com>
2026-05-08 14:55:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from io import BytesIO
 langs_dict = GoogleTranslator().get_supported_languages(as_dict=True)
 
 def main():
-    st.image("PragyanAI_Transperent.png")
+    st.image("img.jpg")
     st.title(" PragyanAI - VVIET Workshop: Audio Hub")
     
     audio_bytes = audio_recorder(text="Click to record", neutral_color="#6aa36f")
